9a97786bd28d61c97c7b9b197483256f3c244e53
thorhan <paezvallejohecthor@gmail.com>
2017-05-27 02:15:18 -0500

    MANTENIMIENTO SUCURSAL BASICO

---
+++ app/Http/Controllers/BasicManage/Sucursal.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Http\Controllers\BasicManage;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\BasicManage\Sucursal;
+use Illuminate\Support\Facades\Validator;
+
+class Sucursal extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');  //Esto debe activarse cuando estemos con sessión
+    }
+
+    public function EditStatus(Request $r )
+    {
+        if ( $r->ajax() ) {
+            Sucursal::runEditStatus($r);
+            $return['rst'] = 1;
+            $return['msj'] = 'Registro actualizado';
+            return response()->json($return);
+        }
+    }
+
+    public function New(Request $r )
+    {
+        if ( $r->ajax() ) {
+            $rules=array(
+                'sucursal' => 'required|max:100|unique:sucursales'
+            );
+
+            $validator=Validator::make($r->all(), $rules);
+
+            if ( !$validator->fails() ) {
+                Sucursal::runNew($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro creado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = '!Sucursal existente¡, modifique su sucursal';
+            }
+            return response()->json($return);
+        }
+    }
+
+    public function Edit(Request $r )
+    {
+        if ( $r->ajax() ) {
+            $rules=array(
+                'sucursal' => 'required|max:100|unique:sucursales,sucursal,'.$r->id
+            );
+
+            $validator=Validator::make($r->all(), $rules);
+
+            if ( !$validator->fails() ) {
+                Sucursal::runEdit($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro actualizado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = '!Sucursal existente¡, modifique su sucursal';
+            }
+            return response()->json($return);
+        }
+    }
+
+    public function Load(Request $r )
+    {
+        if ( $r->ajax() ) {
+            $renturnModel = Sucursal::runLoad($r);
+            $return['rst'] = 1;
+            $return['data'] = $renturnModel;
+            $return['msj'] = "No hay registros aún";
+            return response()->json($return);
+        }
+    }
+
+}
---
+++ app/Models/BasicManage/Sucursal.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Models\BasicManage;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sucursal extends Model
+{
+    protected   $table = 'sucursales';
+
+    public static function runEditStatus($r)
+    {
+        $sucursal = Sucursal::find($r->id);
+        $sucursal->estado = trim( $r->estadof );
+        $sucursal->persona_id_updated_at=1;
+        $sucursal->save();
+    }
+
+    public static function runNew($r)
+    {
+        $sucursal = new Sucursal;
+        $sucursal->sucursal = trim( $r->sucursal );
+        $sucursal->direccion = trim( $r->direccion );
+        $sucursal->telefono = trim( $r->telefono );
+        $sucursal->celular = trim( $r->celular );
+        $sucursal->email = trim( $r->email );
+        $sucursal->estado = trim( $r->estado );
+        $sucursal->persona_id_created_at=1;
+        $sucursal->save();
+    }
+
+    public static function runEdit($r)
+    {
+        $sucursal = Sucursal::find($r->id);
+        $sucursal->sucursal = trim( $r->sucursal );
+        $sucursal->direccion = trim( $r->direccion );
+        $sucursal->telefono = trim( $r->telefono );
+        $sucursal->celular = trim( $r->celular );
+        $sucursal->email = trim( $r->email );
+        $sucursal->estado = trim( $r->estado );
+        $sucursal->persona_id_updated_at=1;
+        $sucursal->save();
+    }
+
+    public static function runLoad($r)
+    {
+        $sql=Sucursal::select('id','sucursal','direccion','telefono','celular','email','estado')
+            ->where( 
+                function($query) use ($r){
+                    if( $r->has("sucursal") ){
+                        $sucursal=trim($r->sucursal);
+                        if( $sucursal !='' ){
+                            $query->where('sucursal','like','%'.$sucursal.'%');
+                        }
+                    }
+                    if( $r->has("direccion") ){
+                        $direccion=trim($r->direccion);
+                        if( $direccion !='' ){
+                            $query->where('direccion','like','%'.$direccion.'%');
+                        }
+                    }
+                    if( $r->has("telefono") ){
+                        $telefono=trim($r->telefono);
+                        if( $telefono !='' ){
+                            $query->where('telefono','like','%'.$telefono.'%');
+                        }
+                    }
+                    if( $r->has("celular") ){
+                        $celular=trim($r->celular);
+                        if( $celular !='' ){
+                            $query->where('celular','like','%'.$celular.'%');
+                        }
+                    }
+                    if( $r->has("email") ){
+                        $email=trim($r->email);
+                        if( $email !='' ){
+                            $query->where('email','like','%'.$email.'%');
+                        }
+                    }
+                    if( $r->has("estado") ){
+                        $estado=trim($r->estado);
+                        if( $estado !='' ){
+                            $query->where('estado','like','%'.$estado.'%');
+                        }
+                    }
+                }
+            );
+        $result = $sql->orderBy('sucursal','asc')->get();
+        return $result;
+    }
+}
