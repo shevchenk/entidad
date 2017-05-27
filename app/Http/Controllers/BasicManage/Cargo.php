8c9649adb4c51f5a03a90b55875cd9cb3e0ea13b
shevchenk <jorgeshevchenk@gmail.com>
2017-05-26 23:05:56 -0500

    actualizando la parte final

--- app/Http/Controllers/BasicManage/Cargo.php
+++ app/Http/Controllers/BasicManage/Cargo.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\BasicManage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\BasicManage\Cargo;
+use Illuminate\Support\Facades\Validator;
 
 class Cargo extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');  //Esto debe activarse cuando estemos con sessión
+    }
+
     public function EditStatus(Request $r )
     {
         if ( $r->ajax() ) {
@@ -20,9 +26,21 @@ class Cargo extends Controller
     public function New(Request $r )
     {
         if ( $r->ajax() ) {
-            Cargo::runNew($r);
-            $return['rst'] = 1;
-            $return['msj'] = 'Registro creado';
+            $rules=array(
+                'cargo' => 'required|max:100|unique:cargos'
+            );
+
+            $validator=Validator::make($r->all(), $rules);
+
+            if ( !$validator->fails() ) {
+                Cargo::runNew($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro creado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = '!Cargo existente¡, modifique su cargo';
+            }
             return response()->json($return);
         }
     }
@@ -30,9 +48,21 @@ class Cargo extends Controller
     public function Edit(Request $r )
     {
         if ( $r->ajax() ) {
-            Cargo::runEdit($r);
-            $return['rst'] = 1;
-            $return['msj'] = 'Registro actualizado';
+            $rules=array(
+                'cargo' => 'required|max:100|unique:cargos,cargo,'.$r->id
+            );
+
+            $validator=Validator::make($r->all(), $rules);
+
+            if ( !$validator->fails() ) {
+                Cargo::runEdit($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro actualizado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = '!Cargo existente¡, modifique su cargo';
+            }
             return response()->json($return);
         }
     }
--- app/Http/Controllers/ExpertManage/Cargo.php
+++ app/Http/Controllers/ExpertManage/Cargo.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers\ExpertManage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\ExpertManage\Cargo;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class Cargo extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth');  //Esto debe activarse cuando estemos con sessión
@@ -27,9 +26,21 @@ class Cargo extends Controller
     public function New(Request $r )
     {
         if ( $r->ajax() ) {
-            Cargo::runNew($r);
-            $return['rst'] = 1;
-            $return['msj'] = 'Registro creado';
+            $rules=array(
+                'cargo' => 'required|max:100|unique:cargos'
+            );
+
+            $validator=Validator::make($r->all(), $rules);
+
+            if ( !$validator->fails() ) {
+                Cargo::runNew($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro creado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = '!Cargo existente¡, modifique su cargo';
+            }
             return response()->json($return);
         }
     }
@@ -37,9 +48,21 @@ class Cargo extends Controller
     public function Edit(Request $r )
     {
         if ( $r->ajax() ) {
-            Cargo::runEdit($r);
-            $return['rst'] = 1;
-            $return['msj'] = 'Registro actualizado';
+            $rules=array(
+                'cargo' => 'required|max:100|unique:cargos,cargo,'.$r->id
+            );
+
+            $validator=Validator::make($r->all(), $rules);
+
+            if ( !$validator->fails() ) {
+                Cargo::runEdit($r);
+                $return['rst'] = 1;
+                $return['msj'] = 'Registro actualizado';
+            }
+            else{
+                $return['rst'] = 2;
+                $return['msj'] = '!Cargo existente¡, modifique su cargo';
+            }
             return response()->json($return);
         }
     }
