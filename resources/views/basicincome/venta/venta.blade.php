e41f0e4a198bb307ff47004ff6944324cf155238
thorhan <paezvallejohecthor@gmail.com>
2017-09-30 00:46:17 -0500

    ve

--- resources/views/basicincome/venta/venta.blade.php
+++ resources/views/basicincome/venta/venta.blade.php
@@ -38,105 +38,105 @@
                 </div>
                 <div class="box-body with-border">
                     <form id="ModalMatriculaForm">
-                        <div class="col-md-12"> <!-- INICIO COLD MD 12-->
+
+                    <div class="col-md-12"> <!-- INICIO COLD MD 12-->
                             <div class="panel panel-primary"> <!-- INICIO PANEL PRIMARY-->
                                 <div class="panel-heading"><center>Registrar Venta</center></div>  <!-- SOLO ES EL NOMBRE CON LA SOMBRA-->
-
                                 <div class="panel-body"> <!-- INICIO PANEL BODY-->
 
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label>Categoria</label>
-                                            <select  class="form-control selectpicker show-menu-arrow" data-live-search="true" id="slct_categoria_id" name="slct_categoria_id">
-                                                <option value="0">.::Seleccione::.</option>
-                                            </select>
-                                        </div> 
-                                    </div>
+                            <div class="col-md-3"> <!-- INICIO COLD MD 12-->
+                                <div class="panel panel-primary"> <!-- INICIO PANEL PRIMARY-->                                   
+                                    <div class="panel-body"> <!-- INICIO PANEL BODY-->
 
-                                    
-
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label>Articulo</label>
-                                            <select  class="form-control selectpicker show-menu-arrow" data-live-search="true" id="slct_articulo_id" name="slct_articulo_id">
-                                                <option value="0">.::Seleccione::.</option>
-                                            </select>
-                                        </div> 
-                                    </div>                                  
-                                    
-                                    
-                                                                  
-                                    
-                                
-                        <div class="col-md-12">
-                            <div class="panel panel-primary"> <!-- INICIO PANEL PRIMARY-->    
-                                <div class="panel-body"> <!-- INICIO PANEL BODY-->    
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <label>Resp. de la Venta</label>
-                                            <input type="hidden" name="txt_responsable_id" id="txt_responsable_id" class="form-control mant" readonly="">
-                                            <input type="text" class="form-control mant" id="txt_responsable" name="txt_responsable" disabled="">
-                                        </div> 
-                                    </div>
-
-                                    <div class="col-md-2">
-                                        <div class="form-group">
-                                            <label>Fecha</label>
-                                            <input type="hidden" class="form-control mant" id="txt_tipo_matricula" name="txt_tipo_matricula" readOnly="" value="1">
-                                            <input type="text" class="form-control mant" id="txt_fecha" name="txt_fecha" readOnly="">
-                                        </div> 
-                                    </div>
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <label>Cliente</label>
-                                            <input type="hidden" name="txt_persona_id" id="txt_persona_id" class="form-control" readonly="">
-                                            <input type="text" class="form-control" id="txt_persona" name="txt_persona" disabled="">
-                                        </div> 
-                                    </div>
-                                    <div class="col-md-1">
-                                        <div class="form-group">
-                                            <label>&nbsp;&nbsp;&nbsp;</label>
-                                            <span class="input-group-btn">
-                                                <button type="button" class="btn btn-info btn-flat" data-toggle="modal" data-target="#ModalListapersona" data-filtros="estado:1" data-personaid="ModalMatriculaForm #txt_persona_id"  data-persona="ModalMatriculaForm #txt_persona"   data-buscaralumno="1">Buscar Persona</button> <!-- data-dni="ModalMatriculaForm #txt_dni" -->
-                                            </span>
-                                        </div> 
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                                
-                                            <div class="col-sm-2">
-                                                 <label>SubTotal:</label>
-                                                <input type="text" class="form-control" id="txt_nro_promocion" name="txt_nro_promocion" placeholder="Nro" disabled>
-                                            </div>
-                                            <div class="col-sm-2">
-                                                <label>IGV:</label>
-                                                <input type="text" class="form-control" id="txt_nro_promocion" name="txt_nro_promocion" placeholder="Nro" disabled>
-                                            </div>
-                                            <div class="col-sm-2">
-                                                <label>Promocion:</label>
-                                                <input type="text" class="form-control" id="txt_nro_promocion" name="txt_nro_promocion" placeholder="Nro" disabled>
-                                            </div>
-
-                                            <div class="col-sm-2">
-                                                <label>Total:</label>
-                                                <input type="text" class="form-control" id="txt_monto_promocion" name="txt_monto_promocion" placeholder="Monto" disabled>
-                                            </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group">
+                                                <label>Categoria</label>
+                                                <select  class="form-control selectpicker show-menu-arrow" data-live-search="true" id="slct_categoria_id" name="slct_categoria_id">
+                                                    <option value="0">.::Seleccione::.</option>
+                                                </select>
+                                            </div> 
                                         </div>
-                                    </div>     
-                                </div><!-- FIN PANEL BODY-->    
-                            </div> <!-- FIN PANEL PRIMARY-->  
-                        </div>
-                                
-                        </div> <!-- FIN PANEL BODY--> 
-         
+                                        
+
+                                        <div class="col-md-8">
+                                            <div class="form-group">
+                                                <label>Articulo</label>
+                                                <select  class="form-control selectpicker show-menu-arrow" data-live-search="true" id="slct_articulo_id" name="slct_articulo_id">
+                                                    <option value="0">.::Seleccione::.</option>
+                                                </select>
+                                            </div> 
+                                        </div>                                  
+                                    </div> <!-- FIN PANEL BODY-->    
+                                </div> <!-- FIN PANEL PRIMARY-->           
+                            </div> <!-- FIN COLD MD 12-->        
+                                                                      
+                                        
+                                    
+                            <div class="col-md-9">
+                                <div class="panel panel-primary"> <!-- INICIO PANEL PRIMARY-->    
+                                    <div class="panel-body"> <!-- INICIO PANEL BODY-->    
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Resp. de la Venta</label>
+                                                <input type="hidden" name="txt_responsable_id" id="txt_responsable_id" class="form-control mant" readonly="">
+                                                <input type="text" class="form-control mant" id="txt_responsable" name="txt_responsable" disabled="">
+                                            </div> 
+                                        </div>
+
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label>Fecha</label>
+                                                <input type="hidden" class="form-control mant" id="txt_tipo_matricula" name="txt_tipo_matricula" readOnly="" value="1">
+                                                <input type="text" class="form-control mant" id="txt_fecha" name="txt_fecha" readOnly="">
+                                            </div> 
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Cliente</label>
+                                                <input type="hidden" name="txt_persona_id" id="txt_persona_id" class="form-control" readonly="">
+                                                <input type="text" class="form-control" id="txt_persona" name="txt_persona" disabled="">
+                                            </div> 
+                                        </div>
+                                        <div class="col-md-1">
+                                            <div class="form-group">
+                                                <label>&nbsp;&nbsp;&nbsp;</label>
+                                                <span class="input-group-btn">
+                                                    <button type="button" class="btn btn-info btn-flat" data-toggle="modal" data-target="#ModalListapersona" data-filtros="estado:1" data-personaid="ModalMatriculaForm #txt_persona_id"  data-persona="ModalMatriculaForm #txt_persona"   data-buscaralumno="1">Buscar Persona</button> <!-- data-dni="ModalMatriculaForm #txt_dni" -->
+                                                </span>
+                                            </div> 
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="form-group">
+                                                    
+                                                <div class="col-sm-2">
+                                                     <label>SubTotal:</label>
+                                                    <input type="text" class="form-control" id="txt_nro_promocion" name="txt_nro_promocion" placeholder="Nro" disabled>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <label>IGV:</label>
+                                                    <input type="text" class="form-control" id="txt_nro_promocion" name="txt_nro_promocion" placeholder="Nro" disabled>
+                                                </div>
+                                                <div class="col-sm-2">
+                                                    <label>Promocion:</label>
+                                                    <input type="text" class="form-control" id="txt_nro_promocion" name="txt_nro_promocion" placeholder="Nro" disabled>
+                                                </div>
+
+                                                <div class="col-sm-2">
+                                                    <label>Total:</label>
+                                                    <input type="text" class="form-control" id="txt_monto_promocion" name="txt_monto_promocion" placeholder="Monto" disabled>
+                                                </div>
+                                            </div>
+                                        </div>     
+                                    </div><!-- FIN PANEL BODY-->    
+                                </div> <!-- FIN PANEL PRIMARY-->  
+                            </div><!-- FIN COLD MD 12-->
+                        </div> <!-- FIN PANEL BODY-->    
+                        </div> <!-- FIN PANEL PRIMARY-->                
+                    </div><!-- FIN COLD MD 12-->    
 
 
-                                   
-                            </div> <!-- FIN PANEL PRIMARY--> 
 
-
-
-                        </div> <!-- FIN COLD MD 12-->
+                        
 
 
                         <div class="form-group"> 
